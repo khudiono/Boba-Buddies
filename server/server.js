af6fd908cbe86fe7360c83c66d4cb492050be704
Kelly Hudiono <kellyhudiono@gmail.com>
2019-03-18 19:36:33 -0700

    add yelp api calls + add images

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const apiKey = require('../config.js').YelpApiKey;
+const yelp = require('yelp-fusion');
+const client = yelp.client(apiKey);
 
 app.use(cors());
 
 app.use(express.static(__dirname + '/../public'));
+
+app.get('/search', (req,res) => {
+  console.log('body', req.query.location)
+  let url = 'https://api.yelp.com/v3/businesses/search';
+  let params = {
+    term: 'bubble tea',
+    location: req.query.location,
+    limit: 3,
+  }
+  client.search(params).then( response => {
+    return response.jsonBody.businesses;
+  })
+  .then(results => {
+    res.status(200).send(results);
+  })
+  .catch(err => {
+    res.status(500).send(err);
+  })
+})
 
 const PORT = 3000;
 
